a74130c0819db5959a0ef845495e4eac91a975e3
Byron <jamesblocker81@gmail.com>
2018-01-26 09:51:13 -0800

    add in obj info

---
+++ script.js
@@ -0,0 +1,23 @@
+var mother = {
+    hairColor: "brown",
+    eyeColor: "brown",
+    bestQuality: "kindness",
+    petPeeve: "untidyness",
+    favoriteFood: "apples"
+};
+
+var father = {
+    hairColor: "black",
+    eyeColor: "brown",
+    bestQuality: "friendly",
+    petPeeve: "lateness",
+    favoriteFood: "tuna"
+};
+
+var me = {
+    hairColor: father.hairColor,
+    eyeColor: father.eyeColor,
+    bestQuality: mother.bestQuality,
+    petPeeve: father.petPeeve,
+    favoriteFood: father.favoriteFood
+};
